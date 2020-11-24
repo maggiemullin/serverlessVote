e387c40b9db4392a758619c498addfe7cfff50a6
maggiemullin <maggiemullin1@gmail.com>
2020-11-24 09:02:46 -0500

    new

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,24 @@
+let votesArray = [0, 0, 0, 0];
+
 // functions/hello.js
 exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
+    const v = event.queryStringParameters.vote
+      if(vote.value === "a") {
+        votesArray[0] += 1;
+      } else if (v === "b") {
+        votesArray[1] += 1;
+      } else if (v === "c") {
+        votesArray[2] += 1;
+      } else if (v === "d") {
+        votesArray[3] += 1;
+      } else if (v === "r") {
+        votesArray = [0, 0, 0, 0];
+      }
+      console.log(JSON.stringify(votesArray));
+    
     return {
       statusCode: 200,
-      body: `Hello ${subject}!`,
+      headers : { 'Allow-Control-Allow-Origin' : '*'},
+      body: JSON.stringify(votesArray),
     }
   }
